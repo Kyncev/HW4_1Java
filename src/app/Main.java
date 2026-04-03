5fd1cae5ba065f70dd00ab56c7d1eed8eecb8a18
Kyncev <45149572+Kyncev@users.noreply.github.com>
2026-04-04 01:41:01 +0300

    Add files via upload

---
+++ src/app/Main.java
@@ -0,0 +1,30 @@
+package app;
+public class Main {
+    public static void main(String[] args) {
+        int productNo;
+        int amountDays;
+        double daySale;
+        double total;
+        String productType;
+        String currency;
+
+        productNo = 1;
+        productType = "smartphone";
+        daySale = 2430.68;
+        amountDays = 5;
+        total = daySale * amountDays;
+        currency = "EUR";
+        System.out.println("Product No " + productNo +  ":" + " " + productType + ",");
+        System.out.println("total sales for "  + amountDays +  " is " + currency + " " + total + ",");
+        System.out.println("sales by day is " + currency + " " + daySale + ",");
+        productNo = 2;
+        productType = "laptop";
+        currency = "EUR";
+        daySale = 1498.12;
+        amountDays = 7;
+        total = daySale * amountDays;
+        System.out.println("Product No " + productNo +  ":" + " " + productType + ",");
+        System.out.println("total sales for "  + amountDays +  " is " + currency + " " + total + ",");
+        System.out.println("sales by day is " + currency + " " + daySale + ",");
+    }
+}
