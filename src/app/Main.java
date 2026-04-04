6b3c7f2a7b671bdac74a8767d04a3e34337f7ed6
Kyncev <45149572+Kyncev@users.noreply.github.com>
2026-04-04 17:11:26 +0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,22 +9,24 @@ public class Main {
         String currency;
 
         productNo = 1;
-        productType = "smartphone";
-        daySale = 2430.68;
+        productType = "smartphone";;
         amountDays = 5;
-        total = daySale * amountDays;
+        total = 12153.41;
+        daySale = total / amountDays;
+        String result = String.format("%.2f", daySale);
         currency = "EUR";
         System.out.println("Product No " + productNo +  ":" + " " + productType + ",");
-        System.out.println("total sales for "  + amountDays +  " is " + currency + " " + Math.round(total) + ",");
-        System.out.println("sales by day is " + currency + " " + Math.round(daySale) + ",");
+        System.out.println("total sales for "  + amountDays +  " days " + " is " + currency + " " + total + ",");
+        System.out.printf("sales by day is " + currency + " " + result + ",");
         productNo = 2;
         productType = "laptop";
         currency = "EUR";
-        daySale = 1498.12;
+        total = 10486.85;
+        daySale = total / amountDays;
         amountDays = 7;
-        total = daySale * amountDays;
+        System.out.println(" ");
         System.out.println("Product No " + productNo +  ":" + " " + productType + ",");
-        System.out.println("total sales for "  + amountDays +  " is " + currency + " " + Math.round(total) + ",");
-        System.out.println("sales by day is " + currency + " " + Math.round(daySale) + ",");
+        System.out.println("total sales for "  + amountDays +  " days " + " is " + currency + " " + total + ",");
+        System.out.printf("sales by day is " + currency + " " + daySale + ",");
     }
 }
